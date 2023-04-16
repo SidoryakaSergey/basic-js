e5f2345755f0f2bd80ee80ae1b342d9185fd96cd
Thanatus <s.sidoryaka@gmail.com>
2023-04-17 01:21:52 +0300

    feat: add half solution Extended repeater

--- src/!temp.js
+++ src/!temp.js
@@ -110,4 +110,76 @@ function transform(arr) {
   });
 }
 
-console.log(transform([1, '--discard-next', 2, 3, '--discard-next', 5, 6]));
+// console.log(transform([1, '--discard-next', 2, 3, '--discard-next', 5, 6]));
+
+function repeater(str, options) {
+  if (!str) {
+    throw new Error('Incorrect arguments!');
+  }
+  let result = '';
+  let add = '';
+
+  let repeatTimes = 0;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 0;
+  let additionSeparator = '|';
+
+  if (!options) {
+    return str;
+  }
+
+  if (options.hasOwnProperty('repeatTimes')) {
+    repeatTimes = options.repeatTimes;
+  }
+  if (options.hasOwnProperty('separator')) {
+    separator = options.separator;
+  }
+  if (options.hasOwnProperty('addition')) {
+    addition = options.addition;
+  }
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  if (options.hasOwnProperty('additionSeparator')) {
+    additionSeparator = options.additionSeparator;
+  }
+  console.log('str= ', str);
+  console.log('separator=', separator);
+  for (let i = 0; i < repeatTimes; i++) {
+    add = '';
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      if (j === additionRepeatTimes - 1) {
+        add += addition;
+      } else {
+        add += addition + additionSeparator;
+      }
+      console.log('add = ', add);
+    }
+    if (i === repeatTimes - 1) {
+      result += str + add;
+    } else {
+      result += str + add + separator;
+    }
+  }
+  if (repeatTimes === 0) {
+    result += str;
+  }
+  if (additionRepeatTimes === 0) {
+    result += addition;
+  }
+  return result;
+}
+
+console.log(
+  repeater('i7v9VAiIYf', {
+    repeatTimes: 1,
+    separator: 'EJVVKb7ADk',
+    addition: 'FeBa2b7RLL',
+    additionRepeatTimes: 2,
+    additionSeparator: 'TEn9kEcy0M',
+  })
+);
+
+// 'i7v9VAiIYfFeBa2b7RLLTEn9kEcy0MFeBa2b7RLL'
+//  i7v9VAiIYfFeBa2b7RLLTEn9kEcy0MFeBa2b7RLL
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,77 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!str) {
+    throw new Error('Incorrect arguments!');
+  }
+  let result = '';
+  let add = '';
+
+  let repeatTimes = 0;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 0;
+  let additionSeparator = '|';
+
+  if (!options) {
+    return str;
+  }
+
+  if (options.hasOwnProperty('repeatTimes')) {
+    repeatTimes = options.repeatTimes;
+  }
+  if (options.hasOwnProperty('separator')) {
+    separator = options.separator;
+  }
+  if (options.hasOwnProperty('addition')) {
+    addition = options.addition;
+  }
+  if (options.hasOwnProperty('additionRepeatTimes')) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+  if (options.hasOwnProperty('additionSeparator')) {
+    additionSeparator = options.additionSeparator;
+  }
+  for (let i = 0; i < repeatTimes; i++) {
+    console.log(i);
+    add = '';
+    for (let j = 0; j < additionRepeatTimes; j++) {
+      if (j === additionRepeatTimes - 1) {
+        add += addition;
+      } else {
+        add += addition + additionSeparator;
+      }
+      console.log('add = ', add);
+    }
+    if (i === repeatTimes - 1) {
+      result += str + add;
+    } else {
+      result += str + add + separator;
+    }
+  }
+  if (repeatTimes === 0) {
+    result += str;
+  }
+  if (additionRepeatTimes === 0) {
+    result += addition;
+  }
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
